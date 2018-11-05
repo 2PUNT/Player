fa2e19f5b4df2df5dec89e01562288e1de353e15
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-05 16:09:46 +0100

    Update ShootControl.hpp
    Working on ShootControl, it is nowhere near finished at the moment.

--- ShootControl.hpp
+++ ShootControl.hpp
@@ -4,11 +4,35 @@
 #include "rtos.hpp"
 
 class ShootControl: public rtos::task<>, public IRunGameTask{
+private:
+	rtos::flag StartFlagShoot;
+	rtos::channel<int, 5> PressedButtonsQueue;
+	rtos::channel<int, 5> ReleasedButtonsQueue;
+	rtos::flag GameOverFlagShoot;
+	rtos::timer ShootTimer;
+	
+	enum ShootControlStates = { WaitForStart, Idle, Reload };
+	ShootControlStates currentState;
+	
+	PlayerData& playerData;
+	EncodeDecodeMSG& encodeDecoder;
+	DisplayControl& displayControl;
+	SendIrMessageControl& sendIrMessageControl;
+	SpeakerControl& speakerControl;
+	
+	int buttonID;
 public:
+	ShootControl(const unsigned int priority, const char* taskName, PlayerData& _playerData, EncodeDecodeMSG& _encodeDecoder, DisplayControl& _displayControl, SendIrMessageControl& _sendIrMessageControl, SpeakerControl& _speakerControl):
+		task(priority, taskName), StartFlagShoot(this, "StartFlagShoot"), PressedButtonsQueue(this, "PressedButtonsQueue"), ReleasedButtonsQueue(this, "ReleasedButtonsQueue"), GameOverFlagShoot(this, "GameOverFlagShoot"),
+		ShootTimer(this, "ShootTimer"), playerData(_playerData), encodeDecoder(_encodeDecoder), displayControl(_displayControl), sendIrMessageControl(_sendIrMessageControl), speakerControl(_speakerControl)
+		{currentState = WaitForStart;}
+	
 	void Start();
 	void GameOver();
 	void ButtonPressed(int ButtonID);
 	void ButtonReleased(int ButtonID);
+	
+	void main();
 };
 
 #endif
