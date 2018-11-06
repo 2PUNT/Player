846e139d03d94baefef93c399de240a8021d63d0
Suirevax <richard.janssen@student.hu.nl>
2018-11-06 15:07:06 +0100

    First Commit RegisterGameParamsControl
    At the moments only does cout's for testing.

---
+++ RegisterGameParamsControl.cpp
@@ -0,0 +1,77 @@
+#include "RegisterGameParamsControl.hpp"
+
+
+void RegisterGameParamsControl::CommandReceived(Message Command){
+	MessagesReceivedRegQueue.write(Command);
+}
+
+void RegisterGameParamsControl::KeyboardKeyPressed(char Key){
+	KeyPressedRegQueue.write(Key);
+}
+
+void RegisterGameParamsControl::main(){
+	STATE S = STATE::WAITING_ON_COMMAND_OR_USER_INPUT;
+	
+	while(true){
+		switch(S){
+			case STATE::WAITING_ON_COMMAND_OR_USER_INPUT:
+			{
+				auto event_t = wait();
+				if(event_t == KeyPressedRegQueue){
+					char Key = KeyPressedRegQueue.read();
+					if(Key == 'A'){
+						S = STATE::WAITING_FOR_PLAYERNUMBER;
+						break;
+					}else if(Key == 'B'){
+						S = STATE::WAITING_FOR_FIREPOWER;
+						break;
+					}else{
+						break;
+					}
+					
+				}else if(event_t == MessagesReceivedRegQueue){
+					auto msg = MessagesReceivedRegQueue.read();
+					if(msg.data != 0){
+						//RemainingTime.set(msg.data);
+						hwlib::cout << "Time set";
+						TimeCheck = 1;
+						break;
+					}else if((msg.data == 0) && TimeCheck && PlayerIDCheck && FirePowerCheck){
+						//ProcessHitControl.start();
+						//GameTimeControl.start();
+						//ShootControl.start();
+						hwlib::cout<< "start";
+						suspend();
+					}else{
+						break;
+					}
+				}
+				break;
+			}
+			case STATE::WAITING_FOR_PLAYERNUMBER:
+			{
+				wait(KeyPressedRegQueue);
+				char Key = KeyPressedRegQueue.read();
+				if((Key > '0') && (Key < ':')){
+					S = STATE::WAITING_ON_COMMAND_OR_USER_INPUT;
+					//PlayerData.setID(Key);
+					hwlib::cout<< "PlayerData = " << Key << "\n";
+					PlayerIDCheck = 1;
+				}
+				break;
+			}
+			case STATE::WAITING_FOR_FIREPOWER:
+			{
+				wait(KeyPressedRegQueue);
+				char Key = KeyPressedRegQueue.read();
+				if((Key > '0') && (Key < ':')){
+					S = STATE::WAITING_ON_COMMAND_OR_USER_INPUT;
+					//PlayerData.setFirepower(Key);
+					hwlib::cout<< "Firepower = " << Key << "\n";
+					FirePowerCheck = 1;
+				}
+				break;
+			}
+		}
+	}
+}
--- RegisterGameParamsControl.hpp
+++ RegisterGameParamsControl.hpp
@@ -2,13 +2,23 @@
 #define PLAYER_REGISTERGAMEPARAMSCONTROL_HPP
 
 #include "rtos.hpp"
-
+#include "ADTs.hpp"
 #include "IKeyboardListener.hpp"
-// De Message ADT is niet beschreven in ADTs.hpp maar deze file gebruikt wel de Message ADT...
-// De juiste file moet dus nog #include worden.
 
 class RegisterGameParamsControl: public rtos::task<>, public IKeyboardListener{
+private:
+	rtos::channel<char,10> KeyPressedRegQueue;
+	rtos::channel<Message,10> MessagesReceivedRegQueue;
+	enum class STATE {WAITING_ON_COMMAND_OR_USER_INPUT, WAITING_FOR_PLAYERNUMBER, WAITING_FOR_FIREPOWER};
+	bool PlayerIDCheck = 0;
+	bool FirePowerCheck = 0;
+	bool TimeCheck = 0;
+	
+	void main();
 public:
+	RegisterGameParamsControl(const unsigned int priority, const char* name):
+	task(priority, name), KeyPressedRegQueue(this, "KeyPressedRegQueue"), MessagesReceivedRegQueue(this, "MessagesReceivedRegQueue"){};
+	
 	void CommandReceived(Message Command);
 	void KeyboardKeyPressed(char Key);
 };
