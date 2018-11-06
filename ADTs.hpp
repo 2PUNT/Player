267d4fc46b34834562cada43e5e77b9af7f93371
Suirevax <richard.janssen@student.hu.nl>
2018-11-06 14:27:11 +0100

    Added Message to ADTs

--- ADTs.hpp
+++ ADTs.hpp
@@ -24,4 +24,13 @@ public:
 	uint16_t ShotDataLength;
 };
 
+struct Message{
+	uint8_t senderID;
+	uint8_t data;
+	
+	Message(int senderID = 0, int data = 0):
+	senderID(senderID), data(data)
+	{};
+};
+
 #endif
