f8cad005c59298163298b3c59b57849d3a1fa855
Brandon Kroes <b.kroes@protonmail.com>
2018-11-07 17:39:50 +0100

    Added the transferdata comments

--- TransferData.hpp
+++ TransferData.hpp
@@ -12,6 +12,7 @@ class TransferDataControl: public rtos::task<>{
 		PersonalComputer & PC;
 
 	public:
+		//! Initialized with all the entity objects and the pc proxy
 		TransferDataControl(
 			PlayerData & pdata,
 			HitDatas & hdata,
@@ -22,6 +23,7 @@ class TransferDataControl: public rtos::task<>{
 			sdata(sdata),
 			PC(PC){}
 
+	//! Gathers all the entity data and sends it to the PC
 	void StartTransfer(){
 		PlayerDataCollection playerData;
 		playerData.PlayerID        = pdata.GetID();
@@ -39,6 +41,8 @@ class PersonalComputer{
 	public:
 		PersonalComputer(){
 		};
+
+		// entity collection assignment so that all the data can be extracted
 		void RegisterData(PlayerDataCollection playerData){
 				seperatorLine();
 				print_intro(playerData.PlayerID);
@@ -53,9 +57,13 @@ class PersonalComputer{
 				seperatorLine();
 		}
 
+
+		///! prints a line to seperate the cout sections
 		void seperatorLine(){
 				hwlib::cout << "========================================================" << hwlib::endl;
 		}
+
+		//! @param player id needs to be given for the introduction
 		void print_intro(int8_t id){
 			hwlib::cout << "Beste "           << id  << hwlib::endl;
 			hwlib::cout << "Je hebt zojuist een potje gespeeld en je krijgt nu de voortgang van het potje te zien." << hwlib::endl;
@@ -63,6 +71,9 @@ class PersonalComputer{
 		}
 
 
+		//! @param PlayerFirePower is used to calculate the wait time for the game
+		//! @param HitDataLength is used to calculate the amount of hits for the waited time
+		//! @param ShotDataLength id needs to be given to know how many times have been shot
 		void print_activities(int8_t PlayerFirePower,	uint16_t HitDataLength,	uint16_t ShotDataLength){
 			hwlib::cout << "De activiteiten log is als volgt: " << hwlib::endl;
 			hwlib::cout << "Je hebt " << ShotDataLength << "aantal keer geschoten en daardoor heb je " << ShotDataLength * PlayerFirePower << " aantal seconden moeten wachten" << hwlib::endl;
@@ -70,7 +81,7 @@ class PersonalComputer{
 			hwlib::cout << "Je bent " << HitDataLength   << "aantal keer beschoten en daardoor heb je " << (HitDataLength * 2)  << "aantal seconden moeten wachten" << hwlib::endl; // TODO: Time defined for hit wait
 		}
 
-
+		//! @param PlayerHealth is used to cout the latest health of player
 		void print_health(int8_t PlayerHealth){
 			hwlib::cout << "tot slot was de eindstand van je health: " << PlayerHealth <<  hwlib::endl;
 		}
