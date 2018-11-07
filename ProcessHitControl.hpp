8c23f83b762fb909968241091bdbb3f416782e46
Suirevax <richard.janssen@student.hu.nl>
2018-11-07 16:11:46 +0100

    Doxygen addedto processhit & registergameparams

--- ProcessHitControl.hpp
+++ ProcessHitControl.hpp
@@ -3,7 +3,15 @@
 
 #include "rtos.hpp"
 #include "ADTs.hpp"
+/// @file
 
+/// \brief
+/// Task for Registering Hits from other players
+/// \details
+/// the function of this task is to set the playernumber, firepower, and time. After all these variables have been set this task starts the game.
+/// The playernumber and firepower are set using an keypad. The playerNumber and firepower are set by pressing A or B Respectivly. Followed by a single digit (that is greater than 0).
+/// The time and the signal to start the game are set by receiving an Message ADT through an rtos channel.
+/// once the game has been started(a start signal has been received) this task wil be put in an eternal suspend.
 class ProcessHitControl: public rtos::task<>, public IRunGameTask{
 	rtos::channel<Message,10> MessagesReceivedRunQueue;
 	rtos::flag StartFlagHit;
@@ -20,9 +28,14 @@ public:
 	task(priority, name), MessagesReceivedRunQueue(this, "MessagesReceivedRunQueue"),
 	StartFlagHit(this, "startFlagHit"), GameOverFlagHit(this, "GameOverFlagHit"),
 	ProcessHitTimer(this, "ProcessHitTimer"){};
-	
+/// \brief
+/// starts this task
 	void Start();
+/// \brief
+/// ends this task
 	void GameOver();
+/// \brief
+/// call this to start registering a hit
 	void HitReceived(Message hit);
 };
 
--- RegisterGameParamsControl.hpp
+++ RegisterGameParamsControl.hpp
@@ -5,13 +5,28 @@
 #include "ADTs.hpp"
 #include "IKeyboardListener.hpp"
 
+/// @file
+
+/// \brief
+/// Task for setting player values
+/// \details
+/// the function of this task is to set the playernumber, firepower, and time. After all these variables have been set this task starts the game.
+/// The playernumber and firepower are set using an keypad. The playerNumber and firepower are set by pressing A or B Respectivly. Followed by a single digit (that is greater than 0).
+/// The time and the signal to start the game are set by receiving an Message ADT through an rtos channel.
+/// once the game has been started(a start signal has been received) this task wil be put in an eternal suspend.
 class RegisterGameParamsControl: public rtos::task<>, public IKeyboardListener{
 private:
 	rtos::channel<char,10> KeyPressedRegQueue;
 	rtos::channel<Message,10> MessagesReceivedRegQueue;
 	enum class STATE {WAITING_ON_COMMAND_OR_USER_INPUT, WAITING_FOR_PLAYERNUMBER, WAITING_FOR_FIREPOWER};
+	/// \brief
+	/// Check if the PlayerID has been changed
 	bool PlayerIDCheck = 0;
+	/// \brief
+	/// Check if the FirePower has been changed
 	bool FirePowerCheck = 0;
+	/// \brief
+	/// Check if the GameTime has been changed
 	bool TimeCheck = 0;
 	
 	void main();
