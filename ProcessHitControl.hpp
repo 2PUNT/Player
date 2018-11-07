ed7c5595ed81c2c542fa02211fbe2ca6def9bdc4
Suirevax <richard.janssen@student.hu.nl>
2018-11-07 10:15:41 +0100

    First commit ProcessHitControl
    Had barely been tested

--- ProcessHitControl.hpp
+++ ProcessHitControl.hpp
@@ -2,12 +2,25 @@
 #define PLAYER_PROCESSHITCONTROL_HPP
 
 #include "rtos.hpp"
-
-// De Message ADT is niet beschreven in ADTs.hpp maar deze file gebruikt wel de Message ADT...
-// De juiste file moet dus nog #include worden.
+#include "ADTs.hpp"
 
 class ProcessHitControl: public rtos::task<>, public IRunGameTask{
+	rtos::channel<Message,10> MessagesReceivedRunQueue;
+	rtos::flag StartFlagHit;
+	rtos::flag GameOverFlagHit;
+	rtos::timer ProcessHitTimer;
+	
+	enum class STATE {WAITING_ON_START, GAME_RUNNING};
+	enum class SUBSTATE {WAITING_ON_HIT, WAITING_ON_TIMER};
+	HitData hit;
+	
+	void main();
 public:
+	ProcessHitControl(const unsigned int priority, const char* name):
+	task(priority, name), MessagesReceivedRunQueue(this, "MessagesReceivedRunQueue"),
+	StartFlagHit(this, "startFlagHit"), GameOverFlagHit(this, "GameOverFlagHit"),
+	ProcessHitTimer(this, "ProcessHitTimer"){};
+	
 	void Start();
 	void GameOver();
 	void HitReceived(Message hit);
