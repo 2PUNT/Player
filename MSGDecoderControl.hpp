b9c83f646e125e5043dfc475e8a237b700325855
Brandon Kroes <b.kroes@protonmail.com>
2018-11-13 10:48:51 +0100

    restoring message channeler

--- MSGDecoderControl.hpp
+++ MSGDecoderControl.hpp
@@ -3,6 +3,9 @@
 
 #include "rtos.hpp"
 #include "EncodeDecodeMSG.hpp"
+#include "ADTs.hpp"
+#include "RegisterGameParamsControl.hpp"
+#include "ProcessHitControl.hpp"
 
 struct Record{
     uint16_t message;
@@ -11,10 +14,20 @@ struct Record{
 
 class MessageChanneler{
 private:
+    Message m;
+	RegisterGameParamsControl& RegGame;
+	ProcessHitControl& HitControl;
 public:
-    MessageChanneler(){};
+    MessageChanneler(RegisterGameParamsControl& _RegGame, ProcessHitControl& _HitControl):
+	RegGame(_RegGame),
+	HitControl(_HitControl)
+	{};
     void SendMessage(Message m){
-      //hwlib::cout << m.data << hwlib::endl;
+        if(m.senderID == 0x00){
+            RegGame.CommandReceived(m);
+        }else{
+            HitControl.HitReceived(m);
+        }
     }
 };
 
@@ -24,20 +37,25 @@ private:
     uint16_t mKnown;
     int n = 0;
     int p;
-    EncodeDecodeMSG & coder;
-    MessageChanneler & msg;
+    Message em;
+    MessageChanneler& Channeler;
+    EncodeDecodeMSG Encode;
+    Record Records[10];
     rtos::channel<int,16> PauseQueue;
     rtos::timer DecoderTimer;
+    enum class state_t {WAIT_FOR_PAUSE}; // TODO: Delete
+    state_t STATE = state_t::WAIT_FOR_PAUSE; // TODO: Delete
+
     bool messageKnown(uint16_t m);
     bool check(uint16_t m);
     void knownMessage(uint16_t m);
     void main();
 public:
     void PauseDetected(int n);
-    MSGDecoderControl(const unsigned int priority, const char* taskName, MessageChanneler & msg, EncodeDecodeMSG & coder):
+    MSGDecoderControl(const unsigned int priority, const char* taskName, MessageChanneler& _Channeler):
         task(priority, taskName),
-        coder(coder),
-        msg(msg),
+		Channeler(_Channeler),
+        Encode(),
         PauseQueue(this, "PauseQueue"),
         DecoderTimer(this, "DecoderTimer")
     {};
