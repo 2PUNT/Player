df0f6e16f6b8e6d50b18cc2a2d32f6156a82e7c6
Brandon Kroes <b.kroes@protonmail.com>
2018-11-20 13:09:56 +0100

    waiting for negative

--- MSGDecoderControl.cpp
+++ MSGDecoderControl.cpp
@@ -48,7 +48,7 @@ void MSGDecoderControl::main(){
             n = 15;
             if(check(m)){
 				      hwlib::cout << "MSGDecoderControl: Check returned true\n";
-              if(messageKnown(m)){
+              if(! messageKnown(m)){
                 auto em =  Encode.DecodeMessage(m);
                 Channeler.SendMessage(em);
                 knownMessage(m);
