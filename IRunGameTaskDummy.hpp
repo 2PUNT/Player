2ce2e9c6c1c55cb6eb6515daac9d317b0273bc91
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-22 15:17:54 +0100

    Added doxygen for IRunGameTaskDummy

--- IRunGameTaskDummy.hpp
+++ IRunGameTaskDummy.hpp
@@ -1,6 +1,7 @@
 #ifndef PLAYER_IRUNGAMETASKDUMMY_HPP
 #define PLAYER_IRUNGAMETASKDUMMY_HPP
 
+/// \brief A dummy object of IRunGameTask that does nothing.
 class IRunGameTaskDummy: public IRunGameTask{
 public:
 	void Start(){}
