9353d8635b5a6072551f641893834167f317c6ee
Suirevax <richard.janssen@student.hu.nl>
2018-11-22 14:49:36 +0100

    Documentatie Shoot & GameTime
    Documentatie added for ShootControl.hpp and UpdateGameTimeControl.hpp

--- ShootControl.hpp
+++ ShootControl.hpp
@@ -11,8 +11,13 @@
 #include "SendIrMessageControl.hpp"
 #include "Led.hpp"
 #include "IButtonListener.hpp"
+/// @file
 
-
+/// \brief
+/// Task for shooting other players
+/// \details
+/// the function of this task is to shoot an IR message when the button is pressed.
+/// the message it sends contains the players PlayerID and Firepower. Which have been encoded using EncodeDecodeMsg
 class ShootControl: public rtos::task<>, public IRunGameTask, public IButtonListener {
 private:
 	rtos::flag StartFlagShoot;
@@ -41,16 +46,46 @@ private:
 	int reloadTime; // time to reload in ms
 	uint16_t shootMessage;
 public:
+	///@fn ShootControl(const unsigned int priority, const char* taskName, PlayerData& _playerData, ShotDatas& _shotDatas, RemainingTime& _remainingTime, EncodeDecodeMSG& _encodeDecoder, Led& _led, SendIrMessageControl& _sendIrMessageControl, SpeakerControl& _speakerControl, int triggerID)
+	///@brief The constructor for the ShootControl class.
+	///@details This contructor creates a ShootControl object.
+	///@param priority Priority of the task.
+	///@param taskName Name of the task.
+	///@param _playerData Entity object where the players data is stored.
+	///@param _shotDatas Entity object where the every shot fired by this player is recorded.
+	///@param _remainingTime Entity object where the remaining game time is stored.
+	///@param _encodeDecoder Application Logic object that is used to encode or decode a message.
+	///@param _led Boundry object that controls an IR-led.
+	///@param _sendIrMessageControl RTOS task that is responsible for sending the IR message
+	///@param _speakerControl RTOS Task that is responsible for controlling the speaker
+	///@param triggerID The ID that is assinged to the Button which is designated for shooting
 	ShootControl(const unsigned int priority, const char* taskName, PlayerData& _playerData, ShotDatas& _shotDatas, RemainingTime& _remainingTime, EncodeDecodeMSG& _encodeDecoder, Led& _led, SendIrMessageControl& _sendIrMessageControl, SpeakerControl& _speakerControl, int triggerID):
 		task(priority, taskName), StartFlagShoot(this, "StartFlagShoot"), PressedButtonsQueue(this, "PressedButtonsQueue"), ReleasedButtonsQueue(this, "ReleasedButtonsQueue"), GameOverFlagShoot(this, "GameOverFlagShoot"),
 		ShootTimer(this, "ShootTimer"), playerData(_playerData), shotDatas(_shotDatas), remainingTime(_remainingTime),encodeDecoder(_encodeDecoder), led(_led), sendIrMessageControl(_sendIrMessageControl), speakerControl(_speakerControl), triggerButtonID(triggerID)
 		{currentState = WaitForStart;}
-
+		
+	///@fn void ShootControl::Start()
+	///@brief Starts this task.
 	void Start();
+	
+	///@fn void ShootControl::GameOver()
+	///@brief ends this task.
 	void GameOver();
+	
+	///@fn void ShootControl::ButtonPressed(int ButtonID);
+	///@brief Receive when button is pressed
+	///@details This function writes the button ID to a channel when the button is pressed
+	///@param ButtonID The ButtonID to write.
 	void ButtonPressed(int ButtonID);
+	
+	///@fn void ShootControl::ButtonReleased(int ButtonID);
+	///@brief Receive when button is released
+	///@details This function writes the button ID to a channel when the button is released
+	///@param ButtonID The ButtonID to write.
 	void ButtonReleased(int ButtonID);
-
+	
+	///@fn void ShootControl::main()
+	///@brief Starts the Task. call using rtos::run in actual the main
 	void main();
 };
 
--- UpdateGametimeControl.hpp
+++ UpdateGametimeControl.hpp
@@ -9,7 +9,14 @@
 #include "ProcessHitControl.hpp"
 #include "TransferData.hpp"
 
+/// @file
 
+/// \brief
+/// Task for keeping the remaining game time
+/// \details
+/// This Task keeps track of the remaining game time and updates it in the RemainingTime Entity Object.
+/// It shows the remainingTime using an 4x7 segement led display
+/// When the remaning time runs out the Task calls the GameOver function to suspend itself, shootcontrol, and hitprocesscontrol.
 class UpdateGameTimeControl: public rtos::task<>, public IRunGameTask{
 private:
 	rtos::flag StartFlagTime; // is set when the game starts
@@ -32,13 +39,36 @@ private:
 	int msToTimeMinutes(int timeMS); // convert the remaining time in ms to displayable minutes.
 
 public:
+	///@fn UpdateGameTimeControl(const unsigned int priority, const char* taskName, RemainingTime& _remainingTime, DigitLedDisplay& _digitLedDisplay, IRunGameTask& _shootControl, IRunGameTask& _processHitControl, SpeakerControl& _speakerControl, TransferDataControl& _transferDataControl)
+	///@brief The constructor for the UpdateGameTimeControl class.
+	///@details This contructor creates a UpdateGameTimeControl object.
+	///@param priority Priority of the task.
+	///@param taskName Name of the task.
+	///@param _remainingTime Entity object where the remaining game time is stored.
+	///@param _digitLedDisplay Boundry object that control an 4x7 SEGMENT LED DISPLAY
+	///@param _shootControl RTOS Task that is responsible for shooting (sending) an IR message
+	///@param _processHitControl RTOS Task that is responsible for processing hits on the player
+	///@param _speakerControl RTOS Task that is responsible for controlling the speaker
+	///@param _transferDataControl RTOS Task that is responsible for displaying information at the end of the game
 	UpdateGameTimeControl(const unsigned int priority, const char* taskName, RemainingTime& _remainingTime, DigitLedDisplay& _digitLedDisplay, IRunGameTask& _shootControl, IRunGameTask& _processHitControl, SpeakerControl& _speakerControl, TransferDataControl& _transferDataControl):
 		task(priority, taskName), StartFlagTime(this, "StartFlagTime"), GameOverFlagTime(this, "GameOverFlagTime"), GameTimeClock(this, 100000, "GameTimeClock"),
 		gameTime(0), maxGameTime(0), startTime(0), remainingTime(_remainingTime), digitLedDisplay(_digitLedDisplay), shootControl(_shootControl), processHitControl(_processHitControl),
 		speakerControl(_speakerControl), transferDataControl(_transferDataControl){CurrentState = WaitForStart;}
+		
+	///@fn void UpdateGameTimeControl::Start()
+	///@brief Starts this task.
 	void Start();
+	
+	///@fn void UpdateGameTimeControl::GameOver()
+	///@brief ends this task.
 	void GameOver();
+	
+	///@fn void UpdateGameTimeControl::main()
+	///@brief Starts the Task. call using rtos::run in actual the main
 	void main();
+	
+	///@fn void UpdateGameTimeControl::SetProcessHitControl()
+	///@brief Sets processHitControl
 	void SetProcessHitControl(IRunGameTask& _processHitControl); // added to work around a circular reference
 };
 #endif
